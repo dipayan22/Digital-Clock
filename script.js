be6262c948a0f99591137ad8331af4dd403f7986
Dipayan Majumder <110282003+dipayan22@users.noreply.github.com>
2023-01-12 17:53:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+setInterval(() => {
+    let hours=document.getElementById("hours")
+    let minites=document.getElementById("minites")
+    let seconds=document.getElementById("seconds")
+    let ampm=document.getElementById("ampm")
+
+    let hh=document.getElementById("hh")
+    let mm=document.getElementById("mm")
+    let ss=document.getElementById("ss")
+
+    let hr_dot=document.querySelector('.hr_dot')
+    let min_dot=document.querySelector('.min_dot')
+    let sec_dot=document.querySelector('.sec_dot')
+
+    let h=new Date().getHours()
+    let m=new Date().getMinutes()
+    let s=new Date().getSeconds()
+    let am=h>=12?"PM" : "AM"
+
+    // convert 24Hr clock to 12Hr clock
+    if(h>12){
+        h=h-12
+    }
+    //add zero before single digit number
+    h= (h<10)? "0"+h:h
+    m= (m<10)? "0"+m:m
+    s= (s<10)? "0"+s:s
+
+    hours.innerHTML=h + "<br><span>Hours</span>"
+    minites.innerHTML=m + "<br><span>Minites</span>" 
+    seconds.innerHTML=s + "<br><span>Seconds</span>" 
+    ampm.innerHTML=am
+
+    hh.style.strokeDashoffset=440-(440*h)/12
+
+    mm.style.strokeDashoffset=440-(440*m)/60
+
+    ss.style.strokeDashoffset=440-(440*s)/60
+
+    hr_dot.style.transfrom = 'rotate($(h*30)deg)';
+
+});
+
